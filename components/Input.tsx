cee72850be9d753f76c859c55ac23c914611bfd7
tokatu4561 <ryobsk4561@gmail.com>
2022-04-30 09:28:23 -0700

    modify input component add value onchange props

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,15 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
 
 interface Props {
   type: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input: FC<Props> = ({ type }) => {
+export const Input: FC<Props> = (props) => {
   return (
     <>
-      <input className="border rounded-sm p-2 w-80" type={type}></input>
+      <input className="border rounded-sm p-2 w-80" {...props}></input>
     </>
   );
 };
