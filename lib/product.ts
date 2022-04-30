797b93d7fdd89e5ff60e08905dc144a76502324b
tokatu4561 <ryobsk4561@gmail.com>
2022-04-29 21:14:11 -0700

    use enviroment value

--- lib/product.ts
+++ lib/product.ts
@@ -1,7 +1,7 @@
 import { IProduct } from "../types/product";
 import { fetchJson } from "./api";
 
-const CMS_URL = "http://localhost:1337";
+const CMS_URL = process.env.CMS_URL;
 
 const stripProduct = (product: IProduct) => {
   return {
