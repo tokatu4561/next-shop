bb2de6b9d0a4ed037e5a847dc7604ff0b5326aff
tokatu4561 <ryobsk4561@gmail.com>
2022-04-30 22:12:47 -0700

    add sign in page and sign in api

---
+++ pages/api/login.tsx
@@ -0,0 +1,37 @@
+import { NextApiHandler } from "next";
+import cookie from "cookie";
+import { fetchJson } from "../../lib/api";
+
+const CMS_URL = process.env.CMS_URL;
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const { jwt, user } = await fetchJson(`${CMS_URL}/auth/local`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ identifier: email, password }),
+    });
+
+    res
+      .status(200)
+      .setHeader(
+        "Set-Cookie",
+        cookie.serialize("jwt", jwt, { path: "api", httpOnly: true })
+      )
+      .json({
+        id: user.id,
+        name: user.username,
+      });
+  } catch (error) {
+    res.status(401).end();
+  }
+};
+
+export default handler;
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,20 +1,62 @@
 import type { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 import { Button } from "../components/Button";
 import { Field } from "../components/Filed";
 import { Input } from "../components/Input";
 import { Layout } from "../components/Layout";
+import { fetchJson } from "../lib/api";
 
 const SignIn: NextPage = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setError(false);
+    setLoading(true);
+
+    try {
+      const response = await fetchJson("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      setLoading(false);
+      router.push("/");
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
   return (
     <Layout title="Sign In">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email" />
+          <Input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Field>
-        <Button type="submit">Sign In</Button>
+        {error && (
+          <p className="text-red-700">
+            メールアドレス、もしくはパスワードが間違っています。
+          </p>
+        )}
+        {loading ? <p>Loading ...</p> : <Button type="submit">Sign In</Button>}
       </form>
     </Layout>
   );
