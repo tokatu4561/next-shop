5d194a5d940542e05ef37315a5cceb015e63c57e
tokatu4561 <ryobsk4561@gmail.com>
2022-04-30 23:08:17 -0700

    modiify nav bar component feat signin signout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,24 @@
 import Link from "next/link";
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useEffect, useState } from "react";
+import { fetchJson } from "../lib/api";
+import { IUser } from "../types/user";
 
 export const NavBar: FC = () => {
-  const user = { name: "Alice" };
+  const [user, setUser] = useState<IUser>();
+
+  useEffect(() => {
+    async () => {
+      try {
+        const user = await fetchJson("/api/user");
+        setUser(user);
+      } catch (error) {}
+    };
+  }, []);
+
+  const handleSignOut = async () => {
+    await fetchJson("/api/logout");
+    setUser(undefined);
+  };
 
   return (
     <nav className="p-4 text-sm">
@@ -14,7 +30,7 @@ export const NavBar: FC = () => {
           <>
             <li className="space-x-4">
               <span>{user.name}</span>
-              <button>Sign Out</button>
+              <button onClick={handleSignOut}>Sign Out</button>
             </li>
           </>
         ) : (
